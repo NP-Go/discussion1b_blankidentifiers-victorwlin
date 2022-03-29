47e66b3f90571e8bacfd1431f067f60605d37031
Victor Lin <linvictorw@gmail.com>
2022-03-29 18:21:07 +0800

    Back to original state

--- blankIdentifiers.go
+++ blankIdentifiers.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	stringA := "This is the message to print!"
 
-	count, err := fmt.Println(stringA)
-	fmt.Println(count, err)
+	// count, err := fmt.Println(stringA)
+	// fmt.Println(count, err)
 
-	// _, err := fmt.Println(stringA)
-	// fmt.Println(err)
+	_, err := fmt.Println(stringA)
+	fmt.Println(err)
 }
